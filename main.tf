5e3bee125ccacfc1f2435bc226d6fcf60b082def
Adnan Said <adnansaid@pc-205-235.customer.ask4.lan>
2023-03-21 23:01:15 +0000

    main/fmt after running 'terraform fmt' the terraform files were correctly reformated

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # Grab vpc module from terraform modules
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name                 = "mighty-rds-vpc"
+  name = "mighty-rds-vpc"
 
-   # Choose an uncommon IP Address range to make IP address collisions more uncommon in the future
+  # Choose an uncommon IP Address range to make IP address collisions more uncommon in the future
   # For VPC peerings, external private connections etc
   # /24 and /27 chosen to have enough space for resources but not too large to overuse IP Address space
 
@@ -24,12 +24,12 @@ module "vpc" {
 #create a random password generator so the password isn't stored in the repository 
 #https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/password
 
-resource "random_password" "RDS_mighty_password" { 
+resource "random_password" "RDS_mighty_password" {
   length  = 16
   special = true
 }
 
-resource "aws_secretsmanager_secret" "RDS_stored_pass" { 
+resource "aws_secretsmanager_secret" "RDS_stored_pass" {
   name = "RDS_stored_pass"
 }
 

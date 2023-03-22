e4e806079dc27eed53c0f09c39edcd90c856a762
Adnan Said <adnansaid@pc-205-235.customer.ask4.lan>
2023-03-22 13:06:41 +0000

    main/vars.tf added new file

---
+++ vars.tf
@@ -0,0 +1,31 @@
+variable "vpc_name" {
+  
+}
+
+variable "subnet_name" {
+  
+}
+
+variable "secret_name" {
+  
+}
+
+variable "rds_username" {
+  
+}
+
+variable "db_name" {
+  
+}
+
+variable "engine" {
+  
+}
+
+variable "engine_version" {
+  
+}
+
+variable "instance_class" {
+  
+}
